53279505bd1885a23e01fb068c747b83a699f8ba
a-lenz <armando.lenz@ctu.unibe.ch>
2018-11-21 15:56:10 +0100

    add exact method for prec_prop

--- R/descriptive_stats.R
+++ R/descriptive_stats.R
@@ -33,8 +33,8 @@
 #'   \item{sd}{standard deviation}
 #'   \item{n}{sample size}
 #'   \item{prec}{precision (half the width of the conficence interval)}
-#'   \item{lo}{lower end of confidence interval}
-#'   \item{hi}{higher end of confidence interval}
+#'   \item{lwr}{lower end of confidence interval}
+#'   \item{upr}{higher end of confidence interval}
 #'   } augmented with method and note elements.
 #' @examples
 #' prec_mean(mu = 5, sd = 2.5, n = 20)
@@ -60,8 +60,8 @@ prec_mean <- function(mu, sd, n = NULL, prec = NULL, conf.level = 0.95) {
                  sd = sd,
                  n = n,
                  prec = prec,
-                 lo = mu - prec,
-                 hi = mu + prec,
+                 lwr = mu - prec,
+                 upr = mu + prec,
                  #note = "n is number in *each* group",
                  method = "Sample size or precision for mean"),
             class = "power.htest")
@@ -86,10 +86,15 @@ prec_mean <- function(mu, sd, n = NULL, prec = NULL, conf.level = 0.95) {
 #' If more than one method is specified or the method is miss-specified, the
 #' 'score' method will be used.
 #'
+#' #' \code{\link[stats]{uniroot}} is used to solve n for the score and
+#' exact method. Agresti-coull can be abbreviated by ac.
+#'
 #' @param r rate or rate ratio.
 #' @param x number of events
 #' @param method The method to use to calculate precision. Exactly one method
 #'   may be provided. Methods can be abbreviated.
+#' @param tol numerical tolerance used in root finding, the default providing
+#'   (at least) four significant digits
 #' @inheritParams prec_mean
 #' @return Object of class "power.htest", a list of arguments (including the
 #'   computed one) augmented with method and note elements.
@@ -207,20 +212,19 @@ prec_rate <- function(r, x = NULL, prec = NULL, conf.level = 0.95,
 #' Exactly one of the parameters \code{n, prec} must be passed as NULL, and that
 #' parameter is determined from the other.
 #'
-#' Currently, the wilson, agresti-coull, and wald method are implemented. The
+#' The wilson, agresti-coull, exact, and wald method are implemented. The
 #' wilson mehtod is suggested for small n (< 40), and the agresti-coull method
 #' is suggested for larger n (see reference). The wald method is not suggested,
 #' but provided due to its widely distributed use.
 #'
-#' \code{\link[stats]{uniroot}} is used to solve n for the agresti-coull and
-#' wilson method. Agresti-coull can be abbreviated by ac.
+#' \code{\link[stats]{uniroot}} is used to solve n for the agresti-coull,
+#' wilson, and exact method. Agresti-coull can be abbreviated by ac.
 #'
 #' @param p proportion
 #' @param method The method to use to calculate sample size or precision.
 #'   Exactly one method may be provided. Methods can be abbreviated.
-#' @param tol numerical tolerance used in root finding, the default providing
-#'   (at least) four significant digits
 #' @inheritParams prec_mean
+#' @inheritParams prec_rate
 #' @return Object of class "power.htest", a list of arguments (including the
 #'   computed one) augmented with method and note elements.
 #' @seealso \code{\link[stats]{binom.test}}, \code{\link[binom]{binom.confint}},
@@ -229,10 +233,8 @@ prec_rate <- function(r, x = NULL, prec = NULL, conf.level = 0.95,
 #'   a Binomial Proportion}, Statistical Science, 16:2, 101-117,
 #'   \href{https://doi.org/10.1214/ss/1009213286}{doi:10.1214/ss/1009213286}
 #' @examples
-#' prec_prop(1, 1)
-#' add(10, 1)
 prec_prop <- function(p, n = NULL, prec = NULL, conf.level = 0.95,
-                      method = c("wilson", "agresti-coull", "wald"),
+                      method = c("wilson", "agresti-coull", "exact", "wald"),
                       tol = .Machine$double.eps^0.25) {
   if (sum(sapply(list(n, prec), is.null)) != 1)
     stop("exactly one of 'n', and 'prec' must be NULL")
@@ -244,7 +246,7 @@ prec_prop <- function(p, n = NULL, prec = NULL, conf.level = 0.95,
     method <- "wilson"
   }
 
-  methods <- c("wald", "ac", "agresti-coull", "wilson")
+  methods <- c("wald", "ac", "agresti-coull", "exact", "wilson")
   id <- pmatch(method, methods)
   meth <- methods[id]
   if (meth == "ac")
@@ -254,7 +256,8 @@ prec_prop <- function(p, n = NULL, prec = NULL, conf.level = 0.95,
     meth <- "wilson"
   }
 
-  z <- qnorm((1 + conf.level) / 2)
+  alpha <- (1 - conf.level) / 2
+  z <- qnorm(1 - alpha)
   z2 <- z * z
 
   if (meth == "wald") {
@@ -285,9 +288,6 @@ prec_prop <- function(p, n = NULL, prec = NULL, conf.level = 0.95,
 
   if (meth == "wilson") {
     wil <- quote({(z * sqrt(n) / (n + z2)) * sqrt(p * (1 - p) + z2 / (4 * n))})
-    # wil <- function(n, p, prec) {
-    #   z * sqrt(p * (1 - p) / n + z2 / (4 * n)) / (1 + z2 / n) - prec
-    # }
     if (is.null(prec))
       prec <- eval(wil)
     if (is.null(n)) {
@@ -297,23 +297,50 @@ prec_prop <- function(p, n = NULL, prec = NULL, conf.level = 0.95,
     padj <- (n * p + z2 / 2) / (n + z2)
   }
 
-  lo <- padj - prec
-  hi <- padj + prec
+  if (meth == "exact") {
+    ex <- function(n, p) {
+      x <- p * n
+      lwr <- qbeta(alpha, x, n - x + 1)
+      if (x == 0)
+        lwr <- 0
+      upr <- qbeta(1 - alpha, x + 1, n - x)
+      if (x == 1)
+        upr <- 1
+      prec <- (upr - lwr) / 2
+      list(lwr = lwr,
+           upr = upr,
+           prec = prec)
+    }
+    if (is.null(n)) {
+      n <- uniroot(function(n) ex(n, p)$prec - prec,
+                   c(1, 1e+07), tol = tol)$root
+    }
+    res <- ex(n, p)
+    lwr <- res$lwr
+    upr <- res$upr
+    padj <- p
+    if (is.null(prec))
+      prec <- res$prec
+  } else {
+    lwr <- padj - prec
+    upr <- padj + prec
+  }
 
-  if(lo < 0)
+
+  if(lwr < 0)
     warning("The lower end of the confidence interval is numerically below 0 and non-sensible. Please choose 'wilson' method.")
-  if(hi > 1)
+  if(upr > 1)
     warning("The upper end of the confidence interval is numerically above 1 and non-sensible. Please choose 'wilson' method.")
 
   structure(list(p = p,
                  n = n,
                  prec = prec,
                  padj = padj,
-                 lo = lo,
-                 hi = hi,
-                 #conf.width = conf.width,
+                 lwr = lwr,
+                 upr = upr,
                  note = "padj is the adjusted proportion, from which the ci is calculated.",
-                 method = paste0("Sample size or precision for a proportion with", meth,
-                                ".\n", conf.level * 100, "% confidence level was chosen.")),
+                 method = paste0("Sample size or precision for a proportion with ", meth,
+                                " confidence interval.\n",
+                                conf.level * 100, "% confidence level was chosen.")),
             class = "power.htest")
 }
