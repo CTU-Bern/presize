ea753ad85b21781881136593bc78d3631d9f0978
a-lenz <armando.lenz@ctu.unibe.ch>
2018-11-28 14:46:30 +0100

    Add agresti-caffo method to prec_riskdiff

--- R/differences.R
+++ R/differences.R
@@ -18,10 +18,17 @@
 #' Exactly one of the parameters \code{n1, conf.width} must be passed as NULL,
 #' and that parameter is determined from the other.
 #'
-#' Newcombe (without continuity correction) is implemented as equation 10 in
+#' Newcombe (\code{newcombe}) proposed a confidence interval based on the wilson
+#' score method for the single proportion (see \link{prec_prop}). The confidence
+#' interval without continuity correction is implemented from equation 10 in
 #' Newcombe (1998)
 #'
-#' \code{\link[stats]{uniroot}} is used to solve n for the newcombe method.
+#' Agresti-Caffo (\code{ac}) confidence interval is based on the Wald confidence
+#' interval, adding 1 success to each cell of the 2 x 2 table (see Agresti and
+#' Caffo 2000).
+#'
+#' \code{\link[stats]{uniroot}} is used to solve n for the newcombe and ac
+#' method.
 #'
 #' The function uses \code{\link[base]{expand.grid}} to provide an estimate of n
 #' or conf.width for every possible combination of supplied arguments.
@@ -31,13 +38,18 @@
 #' @param n1 Number of patients in unexposed group
 #' @param r allocation ratio (relative size of unexposed and exposed cohort
 #'   (\code{n1} / \code{n2}))
-#' @param method The method to use to calculate precision. Exactly one method
-#'   may be provided. Methods can be abbreviated.
+#' @param method Exactly one of \code{newcombe} (\emph{default}), \code{ac}
+#'   (Agresti-Caffo), \code{wald}. Methods can be abbreviated.
 #' @inheritParams prec_mean
 #'
-#' @references Newcombe RG (1998) \emph{Interval estimation for the difference
-#'   between independent proportions: comparison of eleven methods}, Statistics
-#'   in Medicine, 17:873-890
+#' @references
+#' Agresti A and Caffo B (2000) \emph{Simple and Effective Confidence Intervals
+#' for Proportions and Differences of Proportions Result from Adding Two
+#' Successes and Two Failures}, The American Statistician, 54(4):280-288
+#'
+#' Newcombe RG (1998) \emph{Interval estimation for the difference between
+#' independent proportions: comparison of eleven methods}, Statistics in
+#' Medicine, 17:873-890
 #'
 #' @examples
 #' # Validate Newcombe (1998)
@@ -45,7 +57,7 @@
 #' prec_riskdiff(p1 = 10/10, p2 = 0/10, n1 = 10, met = "newcombe")  # Table IIh
 prec_riskdiff <- function(p1, p2, n1 = NULL, conf.width = NULL,
                      r = 1, conf.level = 0.95,
-                     method = c("newcombe", "wald"),
+                     method = c("newcombe", "ac", "wald"),
                      tol = .Machine$double.eps^0.25) {
   if (sum(sapply(list(n1, conf.width), is.null)) != 1)
     stop("exactly one of 'n1', and 'conf.width' must be NULL")
@@ -62,7 +74,7 @@ prec_riskdiff <- function(p1, p2, n1 = NULL, conf.width = NULL,
     method <- "newcombe"
   }
 
-  methods <- c("newcombe", "wald")
+  methods <- c("newcombe", "ac", "wald")
   id <- pmatch(method, methods)
   meth <- methods[id]
   if (is.na(id)) {
@@ -133,6 +145,33 @@ prec_riskdiff <- function(p1, p2, n1 = NULL, conf.width = NULL,
     }
       lwr <- delta - prec
       upr <- delta + prec
+  }
+
+  if (meth == "ac") {
+    ac <- quote({
+      n2 <- n1 / r
+      x1 <- p1 * n1
+      x2 <- p2 * n2
+      .x1 <- x1 + 1
+      .x2 <- x2 + 1
+      .n1 <- n1 + 2
+      .n2 <- n2 + 2
+      .p1 <- .x1 / .n1
+      .p2 <- .x2 / .n2
+      z * sqrt(.p1 * (1 - .p1) / .n1 + .p2 * (1 - .p2) / .n2)
+    })
+    if (is.null(conf.width)) {
+      prec <- eval(ac)
+      conf.width <- prec * 2
+    }
+    if (is.null(n1)) {
+      f <- function(p1, p2, r, prec) uniroot(function(n1) eval(ac) - prec,
+                              c(1, 1e+07), tol = tol)$root
+      n1 <- mapply(f, p1 = p1, p2 = p2, r = r, prec = prec)
+      n2 <- n1 / r
+    }
+    lwr <- delta - prec
+    upr <- delta + prec
   }
 
   structure(list(p1 = p1,
