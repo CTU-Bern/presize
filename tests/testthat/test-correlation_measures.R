ae169f1eceb0fa60f939a79d6e4c063cfab8a828
aghaynes <aghaynes@gmail.com>
2019-07-08 14:26:31 +0200

    example from Bonett 2002

--- tests/testthat/test-correlation_measures.R
+++ tests/testthat/test-correlation_measures.R
@@ -16,3 +16,30 @@ test_that("icc", {
   expect_equal(p$conf.level, 0.95)
   expect_equivalent(p$conf.width, 0.3233102)
 })
+
+test_that("icc (Bonett example)", {
+  rho <- .85
+  k <- 4
+  n <- 25
+  p <- prec_icc(rho, k, n)
+
+  expect_equal(class(p), "presize")
+  expect_equal(p$rho, rho)
+  expect_equal(p$k, k)
+  expect_equal(p$n, n)
+  expect_equal(p$conf.level, 0.95)
+  expect_equivalent(round(p$conf.width, 1), 0.2)
+
+  p <- prec_icc(rho, k, conf.width = .2)
+  expect_equal(class(p), "presize")
+  expect_equal(p$rho, rho)
+  expect_equal(p$k, k)
+  expect_equal(p$n, 20)
+  expect_equal(p$conf.level, 0.95)
+  expect_equivalent(p$conf.width, 0.2)
+
+})
+
+
+
+
