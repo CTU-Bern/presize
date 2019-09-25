4425bfee4c2229777f08c2a5aa1d9782cc5aec71
aghaynes <aghaynes@gmail.com>
2019-09-25 10:00:57 +0200

    prec_rate tests

--- tests/testthat/test-descriptive_stats.R
+++ tests/testthat/test-descriptive_stats.R
@@ -6,25 +6,25 @@ test_that("no errors with normal settings", {
   expect_error(prec_mean(mu = 5, sd = 2.5, conf.width = 2.34),
                regexp = NA)
 
-  # expect_warning(prec_rate(2.5, x = 20))
-  # expect_warning(prec_rate(2.5, conf.width = 2.243))
-  #
-  # expect_error(prec_rate(2.5, x = 20, method = "score"),
-  #              regexp = NA)
-  # expect_error(prec_rate(2.5, conf.width = 2.243, method = "score"),
-  #              regexp = NA)
-  # expect_error(prec_rate(2.5, x = 20, method = "vs"),
-  #              regexp = NA)
-  # expect_error(prec_rate(2.5, conf.width = 2.243, method = "vs"),
-  #              regexp = NA)
-  # expect_error(prec_rate(2.5, x = 20, method = "exact"),
-  #              regexp = NA)
-  # expect_error(prec_rate(2.5, conf.width = 2.243, method = "exact"),
-  #              regexp = NA)
-  # expect_error(prec_rate(2.5, x = 20, method = "wald"),
-  #              regexp = NA)
-  # expect_error(prec_rate(2.5, conf.width = 2.243, method = "wald"),
-  #              regexp = NA)
+  expect_warning(prec_rate(2.5, x = 20))
+  expect_warning(prec_rate(2.5, conf.width = 2.243))
+
+  expect_error(prec_rate(2.5, x = 20, method = "score"),
+               regexp = NA)
+  expect_error(prec_rate(2.5, conf.width = 2.243, method = "score"),
+               regexp = NA)
+  expect_error(prec_rate(2.5, x = 20, method = "vs"),
+               regexp = NA)
+  expect_error(prec_rate(2.5, conf.width = 2.243, method = "vs"),
+               regexp = NA)
+  expect_error(prec_rate(2.5, x = 20, method = "exact"),
+               regexp = NA)
+  expect_error(prec_rate(2.5, conf.width = 2.243, method = "exact"),
+               regexp = NA)
+  expect_error(prec_rate(2.5, x = 20, method = "wald"),
+               regexp = NA)
+  expect_error(prec_rate(2.5, conf.width = 2.243, method = "wald"),
+               regexp = NA)
 
   expect_warning(prec_prop(p = 1:9 / 10, n = 100))
   expect_warning(prec_prop(p = 1:9 / 10, conf.width = .192))
