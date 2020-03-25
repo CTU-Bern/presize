ec11034b20d0dfbd4316f722a1e9b37d4532929e
aghaynes <aghaynes@gmail.com>
2020-03-25 14:37:41 +0100

    add documentation for launch_presize_app

--- R/shinyApp.R
+++ R/shinyApp.R
@@ -1,6 +1,10 @@
 
-
+#' Presize shiny app
+#' Besides the programmatic approach to using presize, we also supply a shiny app.
+#' @usage
+#' launch_presize_app()
 #' @export
+#' @import shiny ggplot2
 
 launch_presize_app <- function(){
   shiny::runApp(system.file("shinyApp", package = "presize"))
