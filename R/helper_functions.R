6194774cf836cb691c3b24687e7b8ef156c4083f
aghaynes <aghaynes@gmail.com>
2020-10-05 13:08:57 +0200

    any failure

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -2,7 +2,7 @@
 # Check whether numeric is within specified range lo, hi
 numrange_check <- function(x, lo = 0, hi = 1) {
   param <- substitute(x)
-  if (!is.null(x) && !is.numeric(x) || any(lo > x | x > hi))
+  if (any(!is.null(x) && !is.numeric(x) || any(lo > x | x > hi)))
     stop(paste0("'", param, "' must be numeric in [", lo, ", ", hi, "]"))
 }
 
