b1ed2a678b70374865ba5c1fa9d3a207ef49462c
Alan Haynes <aghaynes@users.noreply.github.com>
2022-08-12 15:49:57 +0200

    do not run shiny example

--- R/shinyApp.R
+++ R/shinyApp.R
@@ -18,7 +18,10 @@
 #' @export
 #' @examples
 #' # launch the app
+#' \dontrun{
 #' launch_presize_app()
+#' }
+#'
 
 launch_presize_app <- function(){
   shiny::runApp(system.file("shinyApp", package = "presize"))
